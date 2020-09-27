98b2a4abfed15410f58ec7993d6b0c7a140b384c
Jules Gribble <gribblej@me.com>
2020-09-27 17:52:56 +0100

    remove commented out code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,21 +18,6 @@ const useStyles = makeStyles({
 export default function Home() {
     const { isAuthenticated, user, isLoading, getAccessTokenSilently } = useAuth0();
     const classes = useStyles();
-    // React.useEffect(() => {
-    //     async function getUserProfile(){
-    //         const token = await getAccessTokenSilently();
-    //         const response = await fetch('https://auth0demo.eu.auth0.com/userinfo', {
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`
-    //             }
-    //         });
-    //         const json = await response.json();
-    //         console.log('user profile', json)
-    //     }
-    //     if(isAuthenticated){
-    //         getUserProfile();
-    //     }
-    // }, [isAuthenticated]);
 
     if(isAuthenticated) {
         if (user !== undefined){
